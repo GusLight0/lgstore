958ef3e29b000d8e7d861b0c5c5c0726e9f91316
guslight <gle09062024@gmail.com>
2025-10-22 10:45:40 -0300

    new product

--- src/shared/products.ts
+++ src/shared/products.ts
@@ -1,4 +1,4 @@
-
+import myProfile from '../../assets/img/camisa-1.jpg';
 export interface Product {
   id: number;
   name: string;
@@ -13,8 +13,8 @@ export const products: Product[] = [
     id: 1,
     name: "Camisa Preta Streetwear",
     price: 89.90,
-    description: "Camisa social premium em algodão egípcio, corte slim fit. Perfeita para ocasiões formais e casuais elegantes.",
-    image: "../../assets/img/camisa-1.jpg",
+    description: "Camisa de malha premium em algodão, estilo streetwear. Perfeita para ocasiões estilosas e elegantes.",
+    image: myProfile,
     category: "Oversized Streetwear"
   }
 ];
